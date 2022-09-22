9b38e173aa65af36a53af2d1669eeaf67c362004
danghai1412 <haipdgch200436@fpt.edu.vn>
2022-09-22 17:07:58 +0700

    ok

---
+++ index.js
@@ -0,0 +1,10 @@
+const { appendFile } = require("fs");
+const http = require("http");
+const port = 3000;
+
+const app = http.createServer((req , res) => {
+    res.write("<h1> Helllo world </h1>");
+    res.write("<h3> Node Js web page has been deployed to Heroku! </h3>");
+    res.end();
+});
+app.listen(port);
